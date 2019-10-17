59e9748e918df9fc4abbc96b01632ceda20b0235
David Bergeron <david.f.bergeron@gmail.com>
2019-10-17 11:14:30 -0600

    Reimplemented Modals with micromodal.js

--- js/main.js
+++ js/main.js
@@ -15,52 +15,54 @@ function clearTabContent() {
   }
 }
 
-const activePoppers = [];
+MicroModal.init('modal-1');
 
-const deleteBtn = document.querySelector('.delete-btn');
-const deleteMsgPop = document.querySelector('.delete-msg-popper');
-const closePop = document.querySelector('.close-popper-btn');
+// const activePoppers = [];
 
-const popInstance = new Popper(deleteBtn, deleteMsgPop, {
-  placement: 'left',
-  modifiers: {
-    computeStyle: {
-      enabled: true,
-      fn(data) {
-        data.styles = {
-          ...data.styles,
-          position: 'fixed',
-          left: `${(window.innerWidth - data.popper.width) / 2}px`,
-          top: '50%',
-          transform: 'translateY(-50%)',
-          height: '25%'
-        };
-        return data;
-      }
-    }
-  }
-});
+// const deleteBtn = document.querySelector('.delete-btn');
+// const deleteMsgPop = document.querySelector('.delete-msg-popper');
+// const closePop = document.querySelector('.close-popper-btn');
 
-deleteBtn.addEventListener('click', e => {
-  if (deleteMsgPop.classList.contains('hidden')) {
-    deleteMsgPop.classList.remove('hidden');
-    forceUpdate(popInstance);
-    activePoppers.push(deleteMsgPop);
-  } else {
-    removePoppers();
-  }
-});
+// const popInstance = new Popper(deleteBtn, deleteMsgPop, {
+//   placement: 'left',
+//   modifiers: {
+//     computeStyle: {
+//       enabled: true,
+//       fn(data) {
+//         data.styles = {
+//           ...data.styles,
+//           position: 'fixed',
+//           left: `${(window.innerWidth - data.popper.width) / 2}px`,
+//           top: '50%',
+//           transform: 'translateY(-50%)',
+//           height: '25%'
+//         };
+//         return data;
+//       }
+//     }
+//   }
+// });
 
-closePop.addEventListener('click', e => {
-  hide(deleteMsgPop);
-});
+// deleteBtn.addEventListener('click', e => {
+//   if (deleteMsgPop.classList.contains('hidden')) {
+//     deleteMsgPop.classList.remove('hidden');
+//     forceUpdate(popInstance);
+//     activePoppers.push(deleteMsgPop);
+//   } else {
+//     removePoppers();
+//   }
+// });
 
-function removePoppers() {
-  for (let popper of activePoppers) {
-    popper.classList.add('hidden');
-  }
-  activePoppers.length = 0;
-}
+// closePop.addEventListener('click', e => {
+//   hide(deleteMsgPop);
+// });
+
+// function removePoppers() {
+//   for (let popper of activePoppers) {
+//     popper.classList.add('hidden');
+//   }
+//   activePoppers.length = 0;
+// }
 
 // window.addEventListener('click', e => {
 //   const target = e.target;
